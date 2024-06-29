76900d2498e97e5525d86312a777ed407aa6b5a3
vvrmatos <victorkolis@duck.com>
2024-06-29 18:03:24 -0300

    simple hello tkinter program to start the playgroung project

--- 00_simple_programs/00_hello_tkinter.py
+++ 00_simple_programs/00_hello_tkinter.py
@@ -13,12 +13,11 @@ label.pack(padx=20, pady=20)
 
 
 # Selectable text
-text_widget = Text(
-    root, height=2, width=30, bg=root.cget("bg"), bd=0, font=("Georgia", 16)
-)
+text_widget = Text(root, height=2, width=30, bg=root.cget("bg"), bd=0, font=("Georgia", 16))
 text_widget.pack(padx=40, pady=40)
 text_widget.insert(END, "Hello, Tkinter!")
 text_widget.config(state=DISABLED)
 
 
 root.mainloop()
+
